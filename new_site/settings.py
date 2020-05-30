036aa0723a99650620babbea8c2bc114337009c2
prozaic <46284934+prozaic@users.noreply.github.com>
2020-05-29 23:46:09 -0400

    first40

--- new_site/settings.py
+++ new_site/settings.py
@@ -180,4 +180,3 @@ AWS_DEFAULT_ACL = None
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
-STATIC_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)

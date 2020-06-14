ec4d60decd07decbf80e634bbc6a2f050554c028
prozaic <46284934+prozaic@users.noreply.github.com>
2020-06-13 21:10:51 -0400

    fdatabase4

--- new_site/settings.py
+++ new_site/settings.py
@@ -90,14 +90,11 @@ WSGI_APPLICATION = 'new_site.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME':'kgoxsemq',
-        'USER': 'kgoxsemq',
-        'PASSWORD': 'o9TpLPDOzAS-ZWJVU9TzknvYF_KRruSz',
-        'HOST': 'ruby.db.elephantsql.com', 
-        'PORT': '5432', 
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+
 
 import dj_database_url
 db_from_env = dj_database_url.config(conn_max_age=600)
